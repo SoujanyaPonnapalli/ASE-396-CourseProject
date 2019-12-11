87fb154b818de0d5064d403129ef790e0e9ab30d
SoujanyaPonnapalli <soujanyap95@gmail.com>
2019-12-11 07:51:08 -0600

    Adding code for non-blocking queue

---
+++ code/nonblocking-queue/utils.h
@@ -0,0 +1,36 @@
+#include <stddef.h>
+
+/* ---------- General ---------- */
+
+enum boolean {
+  false = 0,
+  true = 1
+};
+
+typedef enum boolean boolean_type;
+
+/* ---------- Unsigned types ---------- */
+
+typedef unsigned uint32;
+typedef unsigned long long uint64;
+
+
+/* ---------- Dynamic memory allocation ---------- */
+
+extern void* util_malloc(size_t size);
+extern void util_free(void* ptr);
+
+/* ---------- Simple lock ---------- */
+
+typedef unsigned util_lock_type;
+extern void util_initlock(util_lock_type* lock);
+extern void util_lock(util_lock_type* lock);
+extern void util_unlock(util_lock_type* lock);
+
+/* ---------- Fences ---------- */
+
+extern void util_fence(char* fencetype);
+
+/* ---------- Compare and Swap ---------- */
+
+extern boolean_type util_cas(void* loc, void* old, void* new);
